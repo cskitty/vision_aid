23ca514181d846c0fb062fd86e435d7f1cea0969
k-huawei <katherinehua06@gmail.com>
2021-09-05 12:24:45 -0700

    Settings updated. Added shared preferences to save settings on device

---
+++ lib/user_preferences.dart
@@ -0,0 +1,13 @@
+// import 'user_info.dart';
+//
+// class UserPreferences {
+//   static const myUser = User(
+//     imagePath: '',
+//     name: 'Name',
+//     email: 'blank',
+//     about:'blank',
+//     isDarkMode: false,
+//   );
+//
+//
+// }
